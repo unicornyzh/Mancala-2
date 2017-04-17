d2ed64cd69e4c82f3cd395387c4edeb8f32f7965
hzhiqin <h_zhiqin@yahoo.com>
2017-04-17 15:11:28 -0400

    Add files via upload

--- Mancala/src/Evaluation.java
+++ Mancala/src/Evaluation.java
@@ -2,4 +2,18 @@
  * Created by zhiqinhuang on 4/16/17.
  */
 class Evaluation {
+    int value;
+
+    Evaluation(){
+        this.value=0;
+    }
+
+    /**
+     * update the evaluation value for a known board state
+     * positive value means beneficial to player A
+     * @param boardState
+     */
+    void evaluate(BoardState boardState){
+        this.value=boardState.mancalaA-boardState.mancalaB;
+    }
 }
